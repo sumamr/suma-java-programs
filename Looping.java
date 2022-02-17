fe5c97839c029fadff120beb9b86b4a6f982b5dc
sumamr <99878446+sumamr@users.noreply.github.com>
2022-02-17 16:53:22 +0530

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,18 @@
+package suma;
+
+public class Looping {
+	public static void main(String[] args) {
+		int i;
+		for(i=1;i<=100;i++) {
+			if(i%2!=0) {
+				continue;
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+			
+	}
+	  
+
+}
