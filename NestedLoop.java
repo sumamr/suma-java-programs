0cc854f1403fcec6c85af00332cbd21709e8101a
sumamr <99878446+sumamr@users.noreply.github.com>
2022-02-17 16:57:20 +0530

    Add files via upload

---
+++ NestedLoop.java
@@ -0,0 +1,18 @@
+package suma;
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		int i,j;
+		for(i=4;i>=1;i++)
+		{
+			for(j=1;j>=i;j++)
+			{
+				System.out.print(" !");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
