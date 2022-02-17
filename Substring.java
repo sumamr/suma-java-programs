7eae36df3bc641743bd9cd9fcc516dadd7e80c17
sumamr <99878446+sumamr@users.noreply.github.com>
2022-02-17 17:03:02 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,14 @@
+package suma;
+
+public class Substring {
+
+	public static void main(String[] args) {
+		String s="Shanthiniketan college";
+		System.out.println(s.substring(3));
+		System.out.println(s.substring(4,11));
+		System.out.println(s.indexOf('c'));
+		System.out.println(s.lastIndexOf('E'));
+		
+	}
+
+}
