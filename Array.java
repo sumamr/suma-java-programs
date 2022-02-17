ad3e30e63ceeed7a43dbee46a68747c1d9ec1d3e
sumamr <99878446+sumamr@users.noreply.github.com>
2022-02-17 16:44:19 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+package suma;
+
+public class Array {
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+		System.out.println("Array elements");
+		System.out.println("First element = "+a[0]);
+		System.out.println("second element = "+a[1]);
+		System.out.println("third element = "+a[2]);
+		System.out.println("fourth element = "+a[3]);
+		System.out.println("fifth element = "+a[4]);
+	}
+
+}
