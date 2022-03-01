824565a9c8f012b606be09528013af65bd44692d
sumamr <99878446+sumamr@users.noreply.github.com>
2022-03-01 19:11:23 +0530

    Add files via upload

---
+++ Myfirstproject.java
@@ -0,0 +1,21 @@
+package suma;
+
+import java.util.Scanner;
+
+public class Myfirstproject {
+
+	public static void main(String[] args) {
+		int i;
+		int j;
+		int s=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of i");
+		i=sc.nextInt();
+		System.out.println("Enter the values of j");
+		j=sc.nextInt();
+		s=i+j;
+		System.out.println("addition is "+s);
+
+	}
+
+}
