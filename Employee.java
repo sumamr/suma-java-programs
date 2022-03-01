7ad1699d9c20f2cea0c44ae69ec504336f9707b2
sumamr <99878446+sumamr@users.noreply.github.com>
2022-03-01 19:21:39 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,30 +1,8 @@
-package suma;
-
-import java.util.Scanner;
+package P1;
 
 public class Employee {
-
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String name;
-		int age;
-		float sal;
-		char gen;
-		System.out.println("Enter Employee name");
-		name=sc.nextLine();
-		System.out.println("Enter Employee age");
-		age=sc.nextInt();
-		System.out.println("Enter employee salary");
-		sal=sc.nextFloat();
-		System.out.println("Enter employee gender");
-		gen=sc.next().charAt(0);
-		 
-		System.out.println("EMPLOYRR DETAILS");
-		System.out.println("Name = "+name);
-		System.out.println("Age = "+age);
-		System.out.println("Salary = "+sal);
-		System.out.println("Gender = "+gen);
-
-	}
-
+	public int age;
+	public String name;
+	protected float sal;
+	protected String dept;
 }
