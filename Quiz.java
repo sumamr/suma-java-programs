c152d08e07e9313df506f0cfbad35f0d7ed1bb5d
sumamr <99878446+sumamr@users.noreply.github.com>
2022-03-01 19:19:23 +0530

    Add files via upload

---
+++ Quiz.java
@@ -0,0 +1,15 @@
+package edubridgejavaprgms1;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		
+		int[] a = {0,2,4,1,3};
+		for(int i = 0; i < a.length; i++){
+		    a[i] = a[(a[i] + 3) % a.length];
+		    System.out.println("a["+i+"]="+a[i]);
+		}
+
+	}
+
+}
