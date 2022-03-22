bf6ebcb514fde8ad2492b1849eeefff2c3ee81ef
sumamr <99878446+sumamr@users.noreply.github.com>
2022-03-22 13:52:33 +0530

    Add files via upload

--- Sumofdigits.java
+++ Sumofdigits.java
@@ -1,22 +1,30 @@
-package suma;
+package AllForloopProblems;
 
 import java.util.Scanner;
 
 public class Sumofdigits {
+	static void sumdigit()
+	{
+		int digit;
+		int n;
+		int sum=0;
+		int c=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the digits");
+		n=sc.nextInt();
+		while(n!=0)
+		{
+			digit=n%10;
+			sum=sum+digit;
+			n=n/10;
+			c++;
+		}
+		System.out.println("The sum of digits is ; "+sum);
+		System.out.println("The number of digits you entered is : "+c);
+	}
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int num,sum=0,digit;
-		System.out.println("Enter the number");
-		num=sc.nextInt();
-		while(num!=0)
-		{
-			digit=num%10;
-			sum=sum+digit;
-		    num=num/10;
-		    
-		}
-		System.out.println("Sum of digits of given number is "+sum);
+		Sumofdigits.sumdigit();
 
 	}
 
