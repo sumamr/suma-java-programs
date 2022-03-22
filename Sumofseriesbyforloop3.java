2fb1f375f9d25f59f3d83ef2db5a1e5a64e4b8a1
sumamr <99878446+sumamr@users.noreply.github.com>
2022-03-22 13:52:47 +0530

    Add files via upload

---
+++ Sumofseriesbyforloop3.java
@@ -0,0 +1,23 @@
+package AllForloopProblems;
+//find the sum of 1!+2!+3!+.................
+
+public class Sumofseriesbyforloop3 {
+
+	public static void main(String[] args) {
+		int i,j;
+		int sum=0;
+		int n;
+		for(i=1;i<=3;i++)
+		{
+			n=1;
+			for(j=1;j<=i;j++)
+			{
+				n=n*j;
+			}
+			sum=sum+n;
+		}
+		System.out.println("Sum of series is : "+sum);
+
+	}
+
+}
