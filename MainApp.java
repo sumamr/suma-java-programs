aa5fbaae8e22986d3511d931277fe353949b9778
sumamr <99878446+sumamr@users.noreply.github.com>
2022-04-18 08:27:38 +0530

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,31 +1,38 @@
-package preparedStatements;
+package Studentproject;
 
 import java.util.Scanner;
 
 public class MainApp {
 
+	
+
 	public static void main(String[] args) {
-		//input from user
 		Scanner sc=new Scanner(System.in);
+		//menu from user
 		while(true)
 		{
-			System.out.println("DataBase operations");
-			System.out.println("enter your choice");
-			System.out.println("1.To display Student information");
+			System.out.println("Database operations");
+			System.out.println("Enter your choice");
+			System.out.println("1.To display student Operations");
 			System.out.println("2.Insert Student record");
-			System.out.println("3.Update student information based on id");
-			System.out.println("4.Delete student information based on id");
-			System.out.println("5.Select Student based on ID");
+			System.out.println("3.Update Student information based on id");
+			System.out.println("4.Delete Student information based on id");
+			System.out.println("5.Select based on ID");
 			int ch=sc.nextInt();
 			switch(ch)
 			{
-			case 1:DataBaseOperations.displayRecords();
-			       break;
-			case 2:DataBaseOperations.addRecords();
-			          break;
-			case 3: DataBaseOperations.updateRecords();
-			        break;
-			default: System.out.println("invalid choice");
+				case 1: StudentDataOperations.displayStudentInfo();
+				         break;
+				case 2: StudentDataOperations.insertStudentInfo();
+				         break;
+				case 3: StudentDataOperations.updateStudentInfo();
+				         break;
+				case 4: StudentDataOperations.deleteStudentInfo();
+				         break;
+				case 5: StudentDataOperations.selectStudentInfo();
+				         break;
+				 default: System.out.println("Invalid choice");
+				          break;
 			}
 			System.out.println("Do you want to continue yes/no");
 			String choice=sc.next();
@@ -34,7 +41,8 @@ public class MainApp {
 				break;
 			}
 		}
-		System.out.println("Program terminated");
+		//while end
+		System.out.println("program terminated");
 
 	}
 
