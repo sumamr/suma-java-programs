b2d35de3d0a35b4a178071dcdb3634db8449b264
sumamr <99878446+sumamr@users.noreply.github.com>
2022-04-28 13:52:11 +0530

    Add files via upload

---
+++ SpellChecker.java
@@ -0,0 +1,29 @@
+package com.edu;
+
+public class SpellChecker {
+	//byName Annotation
+/*	public void spellchkFunc()
+	{
+		System.out.println("Spell Checker is Enabled");
+	}
+*/
+	//byType Annotation
+	/*public void spellingchkFunction()
+	{
+		System.out.println("Spelling Checker is Enabled");
+	}
+	*/
+	
+	//by Constructor Annotation
+	/*public void SpellCheckerFunction()
+	{
+		System.out.println("SPELLING CHECKER IS ENABLED");
+	}
+	*/
+	
+	//by reference
+	public void spellcheckFunction()
+	{
+		System.out.println("SPELLING CHECKER IS ENABLED");
+	}
+}
