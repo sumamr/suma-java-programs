45f3aa00b016601c5ee4addf73bc9586caba48d4
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-02 13:21:51 +0530

    Add files via upload

--- MainApp.java
+++ MainApp.java
@@ -1,26 +1,15 @@
 package com.edu;
+
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainApp {
-	
-			public static void main(String[] args)
-			{
-			//get the object
-				ApplicationContext ctx=new ClassPathXmlApplicationContext("Spring.xml");
-				     Employee e1=(Employee) ctx.getBean("empobj");
-				     e1.display();
-				     
-				     Employee e2=(Employee) ctx.getBean("empobj1");
-				     e2.display();
-				     
-				     Employee e3=(Employee) ctx.getBean("empobj2");
-				     e3.display();
-				    
-			}
 
-		}
+	public static void main(String[] args) {
+		ApplicationContext ctx=new AnnotationConfigApplicationContext(AppConfig.class);
+		TecnoSpark5 t=ctx.getBean(TecnoSpark5.class);
+		t.MobileFun();
 
+	}
 
-
-
+}
