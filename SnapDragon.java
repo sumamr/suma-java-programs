d346c6f19e240eb4d9bbd6120fb60d9223c195e0
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-02 13:22:22 +0530

    Add files via upload

--- SnapDragon.java
+++ SnapDragon.java
@@ -1,9 +1,13 @@
 package com.edu;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SnapDragon implements MobileProcessor{
-	public void processor()
-	{
-		System.out.println("WORLD BEST PROCESSOR");
+
+	public void Processor() {
+		System.out.println("Worlds Best Processor");
+		
 	}
 	
 
