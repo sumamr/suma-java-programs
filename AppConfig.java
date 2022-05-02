2ec6e9d3feb9f52c3f983d3323764cbcd52364be
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-02 13:18:07 +0530

    Add files via upload

---
+++ AppConfig.java
@@ -0,0 +1,10 @@
+package com.edu;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.edu")
+public class AppConfig {
+
+}
