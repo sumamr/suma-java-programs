d1eb985c7529e8a746b45775168e03ea16407396
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-02 13:23:32 +0530

    Add files via upload

---
+++ Oppo.java
@@ -0,0 +1,17 @@
+package com.edu;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Oppo  implements MobileProcessor{
+	
+	public void Processor() {
+	System.out.println("World Second best Processor");
+		
+	}
+	
+	
+
+}
