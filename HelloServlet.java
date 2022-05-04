c3ec047f5d999326e7fec6295ffeacc5e03bfaf7
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-04 19:52:13 +0530

    Add files via upload

---
+++ HelloServlet.java
@@ -0,0 +1,56 @@
+package com.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		
+		String un=request.getParameter("uname");
+		String up=request.getParameter("upass");
+		System.out.println("Your Name is " +un);
+		System.out.println("Your password is  "+up);
+		
+		//to print data to the browser
+		PrintWriter out=response.getWriter();
+		PrintWriter outp=response.getWriter();
+		out.println("Your Name is "+un);
+		outp.println("Your Password is "+up);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
