5cf8b8baae68026e693a45784999c7a7589c763f
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-04 19:53:38 +0530

    Add files via upload

---
+++ CheckServlet.java
@@ -0,0 +1,45 @@
+package com.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CheckServlet
+ */
+@WebServlet("/CheckServlet")
+public class CheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		int n=Integer.parseInt(request.getParameter("num"));
+		if(n%2==0) {
+			out.println("Entered number is even");
+		}
+		else
+		{
+			out.println("Entered number is odd");
+		}
+		
+	}
+
+}
