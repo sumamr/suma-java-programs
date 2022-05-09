f8f0543bd54c362ede5e3a26c67671d9bdfc8470
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-09 14:32:28 +0530

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Course {
+	void courseDisplay()
+	{
+		System.out.println("JAVA FULL STACK DEVELOPER");
+	}
+
+}
