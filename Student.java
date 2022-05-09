c604058177d9535a3aa3a882049a107e8039ff15
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-09 14:33:20 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,42 +1,48 @@
-package suma.com;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+package com.example.demo;
 
-@Entity  //create table Student
-@Table(name="edustudent")//different table name in database
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Student {
-	@Id   //primary key
-	@GeneratedValue(strategy=GenerationType.AUTO)//to generate primary key automatically
-	@Column(name="edustid")//not null
-	private int sid;
 	
-	@Column(name="edustname",nullable=false,length=30)
-	private String sname;
-	//generate setter and getter method 
-	//generate toString
-	public int getSid() {
-		return sid;
-	}
-	public void setSid(int sid) {
-		this.sid = sid;
-	}
-	public String getSname() {
-		return sname;
-	}
-	public void setSname(String sname) {
-		this.sname = sname;
-	}
-	@Override
-	public String toString() {
-		return "Student [sid=" + sid + ", sname=" + sname + "]";
+	@Autowired
+	Course  course;
 
+		private int sid;
+		private String name;
+		public Student() {
+			super();
+			
+			System.out.println("Constructor");
 		}
+		public Student(int sid, String name) {
+			super();
+			this.sid = sid;
+			this.name = name;
+		}
+		public int getSid() {
+			return sid;
+		}
+		public void setSid(int sid) {
+			this.sid = sid;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		@Override
+		public String toString() {
+			return "Student [sid=" + sid + ", name=" + name + "]";
+		}
+		public void display()
+		{
+			System.out.println("Student display method");
+		}
+		
 
 	}
-
 
 
