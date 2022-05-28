52f1ff9fc231804c3e68363c6eb0036eec57dce4
sumamr <99878446+sumamr@users.noreply.github.com>
2022-05-28 13:27:00 +0530

    Add files via upload

--- Discount.java
+++ Discount.java
@@ -1,74 +1,34 @@
-package edubridgesuma;
+package com.edu;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-class Showroom
+class DiscountAmount
 {
-	String cname;
-	long mobnum;
-	double cost;
-	double dis;
-	double amount;
-	void input() throws IOException
+	int cost;
+	String name;
+	double dc;
+	double amt;
+	Scanner sc=new Scanner(System.in);
+	void input()
 	{
-		InputStreamReader is=new InputStreamReader(System.in);
-		BufferedReader br=new BufferedReader(is);
-		System.out.println("Enter name of the customer");
-		cname=br.readLine();
-		System.out.println("Enter mobile number");
-		mobnum=Long.parseLong(br.readLine());
-		System.out.println("Enter cost of the item purched");
-		cost=Double.parseDouble(br.readLine());
-		
+	   System.out.println("Enter  Name of the customer");
+	   name=sc.next();
+	   System.out.println("Enter the amount of the article");
+	  amt=sc.nextDouble();
 	}
-	Showroom()
+	void cal()
 	{
-		cname=null;
-		mobnum=0;
-		cost=0.0;
-		dis=0.0;
-		amount=0.0;
+		if(cost<)
 	}
-	void calculate()
-	{
-		if(cost<=10000)
-		{
-			dis=(cost*5)/100;
-		}
-		else if(cost>10000 && cost<=20000)
-		{
-			dis=(cost*10)/100;
-		}
-		else if(cost>20000 && cost <=35000)
-		{
-			dis=(cost*15)/100;
-		}
-		else
-		{
-			dis=(cost*20)/100;
-		}
-		amount=cost-dis;
-	}
-	void display() throws IOException
-	{
-		System.out.println("Customer name is "+cname);
-		System.out.println("Mobile number is "+mobnum);
-		System.out.println("Cost is "+cost);
-		System.out.println("Discount is "+dis);
-		System.out.println("Amount after reducing discount is "+amount);
-	}
+	
+	
+    
 }
 
 public class Discount {
 
-	public static void main(String[] args) throws IOException{
-		Showroom sh=new Showroom();
-		sh.input();
-		sh.calculate();
-		sh.display();
-
+	public static void main(String[] args) {
+		
 
 	}
 
